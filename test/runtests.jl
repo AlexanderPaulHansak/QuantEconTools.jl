350af99bb7514f45633e84519620bc303a3e4236
AlexanderPaulHansak <alexander.hansak@gmx.at>
2024-07-13 23:37:06 +0200

    Added tests for markov functionalities

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,7 @@
 
 using Test, QuantEconTools # This load both the test suite and our Package
 
-out = tauchen(0.9,0.2,3,3).s
 
-@test out == 5  
+# Test some markov functionalities
+@test all(abs.(inv_distr(tauchen(0.9,0.2,3,3).Π).InvD' - inv_distr(tauchen(0.9,0.2,3,3).Π).InvD'*tauchen(0.9,0.2,3,3).Π) .<= 1e-10)
+@test all(abs.(inv_distr(rouwenhorst(0.9,0.2,3).Π).InvD' - inv_distr(rouwenhorst(0.9,0.2,3).Π).InvD'*rouwenhorst(0.9,0.2,3).Π) .<= 1e-10)
