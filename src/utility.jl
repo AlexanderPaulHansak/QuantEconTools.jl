43147f9629304ec32e2aa1e7fddedaa2a5f6a676
AlexanderPaulHansak <alexander.hansak@gmx.at>
2024-07-22 11:08:53 +0200

    Modified CRRAUtility

--- src/utility.jl
+++ src/utility.jl
@@ -6,26 +6,24 @@ abstract type AbstractUtility end
 
 # Consumption utility
 
-@doc doc"""
+"""
 Type used to evaluate log utility. Log utility takes the form
 
-u(c) = \log(c)
+u(c) = log(c)
 
 Additionally, this code assumes that if c < 1e-10 then
 
 u(c) = log(1e-10) + 1e10*(c - 1e-10)
 
 """
-struct LogUtility <: AbstractUtility
-    ξ::Float64
-end
+struct LogUtility <: AbstractUtility end
 
 LogUtility() = LogUtility(1.0)
 
 (u::LogUtility)(c::Float64) =
-    c > 1e-10 ? u.ξ*log(c) : u.ξ*(log(1e-10) + 1e10*(c - 1e-10))
+    c > 1e-10 ? log(c) : (log(1e-10) + 1e10*(c - 1e-10))
 derivative(u::LogUtility, c::Float64) =
-    c > 1e-10 ? u.ξ / c : u.ξ*1e10
+    c > 1e-10 ? 1 / c : 1e10
 
 """
 Type used to evaluate CRRA utility. CRRA utility takes the form
@@ -50,7 +48,6 @@ end
 
 (u::CRRAUtility)(c::Float64) =
     c > 1e-10 ?
-           (c^(1.0 - u.γ) - 1.0) / (1.0 - u.γ) :
-           ((1e-10^(1.0 - u.γ) - 1.0) / (1.0 - u.γ) + 1e-10^(-u.γ)*(c - 1e-10))
+           (c^(1.0 - u.γ) - 1.0) / (1.0 - u.γ) : -1e18
 derivative(u::CRRAUtility, c::Float64) =
     c > 1e-10 ? c^(-u.γ) : 1e-10^(-u.γ)
