8091d592100caf70fb40a50ec25eea302a66212d
AlexanderPaulHansak <alexander.hansak@gmx.at>
2024-07-22 11:18:16 +0200

    Added export of utility functions

--- src/QuantEconTools.jl
+++ src/QuantEconTools.jl
@@ -4,6 +4,9 @@ export
     # Markov functionality
     tauchen, rouwenhorst, stationary_distr
 
+    # Utility function
+    AbstractUtility, LogUtility, CRRAUtility, derivative,
+
 include("MarkovApprox.jl")
 include("utility.jl")
 
