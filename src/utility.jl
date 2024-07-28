f9cc015fc96db7247c6e5cfb0a22bb7b1ae59e31
AlexanderPaulHansak <alexander.hansak@gmx.at>
2024-07-28 09:42:07 +0200

    added labor utility

--- src/utility.jl
+++ src/utility.jl
@@ -26,11 +26,11 @@ derivative(u::LogUtility, c::Float64) =
 """
 Type used to evaluate CRRA utility. CRRA utility takes the form
 
-u(c) = c^(1 - γ) / (1 - γ)
+u(c) = c^(1 - σ) / (1 - σ)
 
-Additionally, this code assumes that if c < 1e-10 then
+Additionally, this code assumes that if c < 0 then
 
-u(c) = -10e18
+u(c) = -1e18
 """
 struct CRRAUtility <: AbstractUtility
     γ::Float64
@@ -45,7 +45,33 @@ struct CRRAUtility <: AbstractUtility
 end
 
 (u::CRRAUtility)(c::Real) =
-    c > 1e-10 ?
-           (c^(1.0 - u.γ) - 1.0) / (1.0 - u.γ) : -1e18
+    c > 0 ?
+           c^(1.0 - u.γ) / (1.0 - u.γ) : -1e18
 derivative(u::CRRAUtility, c::Float64) =
     c > 1e-10 ? c^(-u.γ) : 1e-10^(-u.γ)
+
+
+    # Labor Utility
+
+"""
+Type used to evaluate constant Frisch elasticity (CFE) utility. CFE
+utility takes the form
+
+v(h) =  χ h^(1 + 1/γ) / (1 + 1/γ)
+
+Code assumes input h ∈ [0,1], will not correct for different values.
+"""
+struct CFEUtility <: AbstractUtility
+    γ::Float64
+    χ::Float64
+
+    function CFEUtility(γ, χ=1.0)
+        return new(γ, χ)
+    end
+end
+
+(u::CFEUtility)(h::Float64) =
+    -u.χ * h^(1.0 + 1.0/u.γ)/(1.0 + 1.0/u.γ) 
+derivative(u::CFEUtility, h::Float64) =
+    -u.χ * h^(1.0/u.γ) 
+
